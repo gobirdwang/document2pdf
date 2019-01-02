abf227144f51efe4e64805fd80cbb0e4c2ca65f6
GObirD <chwpf@live.com>
2019-01-02 13:49:35 +0800

    实现了异步调用文件转换的功能

--- src/main/java/com/th/documentconversion/asynchronous/FileUpload.java
+++ src/main/java/com/th/documentconversion/asynchronous/FileUpload.java
@@ -14,13 +14,14 @@ public class FileUpload implements CallBack {
     /**
      * 文件上传后，把任务交给转换，不影响上传功能。
      */
-    public void uploadFiles(){
+    public String uploadFiles(){
         System.out.println("开始上传！");
         new Thread(()->{
             documentConvert.documentConvert(FileUpload.this);
         }).start();
         //文件上传成功，可以进行后续的其他操作，此时转换进行中。
         System.out.println("上传完成！");
+        return "上传结束，做转换的同时，开始做其他的事情~";
     }
 
     @Override
